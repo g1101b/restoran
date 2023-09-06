c650d99642785a0ebc448fbb8b503a096d0ddafb
Hlib Blizniuk <atvelor88@gmail.com>
2023-09-06 19:57:18 +0200

    extracted Footer component into its folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,16 @@
 import React from 'react';
 
-import {Header} from './components'
+import {Header, Footer} from './components'
 import {Home} from './pages'
 
 const App: React.FC = () => {
 	return (
 		<>
-		<Header />
+			<Header />
 			<main className="home">
 				<Home />
 			</main>
-			<footer className="footer">
-				<div className="footer__icon">
-					<img src="" alt="" />
-				</div>
-				<div className="footer__nav">
-					<ul>
-						<li className="footer__nav-link">Home</li>
-						<li className="footer__nav-link">About</li>
-						<li className="footer__nav-link">Menu</li>
-						<li className="footer__nav-link">Reservations</li>
-						<li className="footer__nav-link">Order Online</li>
-						<li className="footer__nav-link">Login</li>
-					</ul>
-				</div>
-				<div className="footer__contact">
-					<ul>
-						<li>Address</li>
-						<li>Phone number</li>
-						<li>Email</li>
-					</ul>
-				</div>
-				<div className="footer__socials">
-					<ul>
-						<li>Address</li>
-						<li>Phone number</li>
-						<li>Email</li>
-					</ul>
-				</div>
-			</footer>
+			<Footer />
 		</>
 	);
 }
