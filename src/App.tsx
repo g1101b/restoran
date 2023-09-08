2fb13a62d94f64bbcc02f1903cda1b1d70031387
Hlib Blizniuk <atvelor88@gmail.com>
2023-09-08 20:34:30 +0200

    added route to confirm page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Routes, Route} from 'react-router-dom'
 
 import {Header, Footer} from './components'
-import {Home, Booking} from './pages'
+import {Home, Booking, ConfirmedBooking} from './pages'
 
 import './styles/index.scss'
 
@@ -14,6 +14,7 @@ const App: React.FC = () => {
 				<Routes>
 					<Route path='/' element={<Home />} />
 					<Route path='/booking' element={<Booking />} />
+					<Route path='/confirmed' element={<ConfirmedBooking />} />
 				</Routes>
 			</main>
 			<Footer />
