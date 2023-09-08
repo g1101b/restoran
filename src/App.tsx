2644a29af39a10348dfdebb781a3f58196d9ed02
Hlib Blizniuk <atvelor88@gmail.com>
2023-09-08 17:14:17 +0200

    cleaded app file

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,14 @@ import {Home, Booking} from './pages'
 
 import './styles/index.scss'
 
-const initializeTimes  = () => {
-	return [
-		'17:00',
-		'18:00',
-		'19:00',
-		'20:00',
-		'21:00',
-	]
-}
-
-const updateTimes = (state, action) => {
-}
-
 const App: React.FC = () => {
-	const [times, dispatch] = React.useReducer(updateTimes, initializeTimes());
-
-	const [availableTimes, setAvailableTimes] = React.useState<string[]>(updateTimes());
-
-
 	return (
 		<>
 			<Header />
 			<main className="home">
 				<Routes>
 					<Route path='/' element={<Home />} />
-					<Route path='/booking' element={<Booking times={availableTimes} changeTimes={setAvailableTimes} />} />
+					<Route path='/booking' element={<Booking />} />
 				</Routes>
 			</main>
 			<Footer />
