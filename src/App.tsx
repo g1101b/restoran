8a9fd1468d4025df9e21e916d844326c24e66d2a
Hlib Blizniuk <atvelor88@gmail.com>
2023-09-08 16:31:39 +0200

    added state and reducer

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,24 @@ import {Home, Booking} from './pages'
 
 import './styles/index.scss'
 
+const initializeTimes  = () => {
+	return [
+		'17:00',
+		'18:00',
+		'19:00',
+		'20:00',
+		'21:00',
+	]
+}
+
+const updateTimes = (state, action) => {
+}
+
 const App: React.FC = () => {
-	const [availableTimes, setAvailableTimes] = React.useState<string[]>([]);
+	const [times, dispatch] = React.useReducer(updateTimes, initializeTimes());
+
+	const [availableTimes, setAvailableTimes] = React.useState<string[]>(updateTimes());
+
 
 	return (
 		<>
